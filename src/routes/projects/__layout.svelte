5e9cee09337c8aaadaabbab31017f361258e890c
eilla1 <git@ella.cx>
2021-12-16 09:01:06 -0800

    Add slots? maybe

---
+++ src/routes/projects/__layout.svelte
@@ -0,0 +1,22 @@
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		max-width: 450px;
+		margin: 0 auto;
+	}
+	h1 {
+		text-transform: uppercase;
+		font-size: 1.5em;
+		font-family: 'Merchant Copy Wide', 'Merchant Copy';
+		text-align: center;
+	}
+
+	img {
+		width: 450px;
+		margin: 0 auto;
+	}
+</style>
