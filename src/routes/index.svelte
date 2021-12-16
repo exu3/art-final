1614c6346c464b7ad5443acdb6a09ae977aa5f4f
eilla1 <git@ella.cx>
2021-12-15 22:26:43 -0800

    refactor: sveltekit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,27 @@
 <script>
-	import Greeting from "./components/Greeting.svelte"
-	import About from "./components/About.svelte"
-	import List from "./components/List.svelte"
-	import Footer from "./components/Footer.svelte"
-	import Ads from "./components/Ads.svelte"
+	import Greeting from '../lib/Greeting.svelte';
+	import About from '../lib/About.svelte';
+	import List from '../lib/List.svelte';
+	import Footer from '../lib/Footer.svelte';
+	import Ads from '../lib/Ads.svelte';
 </script>
 
 <main>
 	<h1>Digital Art Portfolio</h1>
 	<Greeting />
-	<hr/>
+	<hr />
 	<div>
 		<ul class="nav">
 			<li><a href="#projects">Projects</a></li>
 			<li><a href="#about">About</a></li>
 		</ul>
 	</div>
-	<hr/>
+	<hr />
 	<About />
 	<List />
 	<p class="text-center">******** END ********</p>
 	<Footer />
-	<hr/>
+	<hr />
 	<Ads />
 </main>
 
