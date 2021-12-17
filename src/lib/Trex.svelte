0e055c145adb473cb40a43bf38aa174dc0955e7b
eilla1 <git@ella.cx>
2021-12-17 09:41:35 -0800

    Fix dinosaur

--- src/lib/Trex.svelte
+++ src/lib/Trex.svelte
@@ -1,6 +1,6 @@
 <pre>
 &nbsp;              __
-              / _) &nbsp;&nbsp;&nbsp;&nbsp; rawr, says the dino.
+              / _) &nbsp;rawr, says the dino.
      _.----._/ /
     /         /
  __/ (  | (  |
