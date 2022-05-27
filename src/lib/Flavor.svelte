06e4d9544e9ce73cba0c0ee1497146ceedfa92cc
exu <git@ella.cx>
2022-05-27 11:00:16 -0700

    Clean up flavor text

--- src/lib/Flavor.svelte
+++ src/lib/Flavor.svelte
@@ -2,24 +2,15 @@
 	const text = [
 		'Flash plugin failed to load.',
 		'Are you a robot?',
-		'Wahoo!',
-		'Moooooooo.',
-		'Ella eating chair.',
 		'This website is open source!',
 		'✨ 🦕',
 		'The ducks at the park are free.',
-		'When the potatoes are screaming. The potatoes are screaming.',
 		'Get dinoed lol.',
 		'Life is hard, life is stressful',
 		'I need peace and tranquility',
 		'dinosaurbbq.org',
 		'No one is around to help',
-		'Shaking my horse, my horse',
-		'Hell yeah.',
-		'Hack yeah.',
-		'Look, the cows!',
-		'Thinking about the rats.',
-		'🐝------------------'
+		'Hack yeah.'
 	];
 	const index = Math.floor(Math.random() * text.length);
 
